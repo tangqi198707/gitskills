3834766abab8e76e16c51aac9bdfbf25d897c2b1
tangqi <tangqi_1987@sina.com>
2015-05-08 10:27:53 +0800

    update IndexAction.class.php

--- Home/Action/IndexAction.class.php
+++ Home/Action/IndexAction.class.php
@@ -20,4 +20,20 @@ class IndexAction extends Action{
 		else
 			echo json_encode('delete fail',__URL__);
 	}
+
+	public function update()
+	{
+		$rs = D('User')->findUserById($_GET['id']);
+		$this->assign('rs',$rs);
+		$this->display();
+	}
+
+	public function	actionupdate()
+	{
+		$rs = D('User')->updateUserById($_POST['id']);
+		if($rs)
+			$this->success('update succcess',__URL__);
+		else
+			$this->error('update failed',__URL__);
+	}
 }
